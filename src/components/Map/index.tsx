8cebb5b25fb06f44e0e7cff5ea9206f6bda5495d
Felipe Moura <felipe.moura@interprocess.com.br>
2021-07-10 11:22:57 -0300

    Configura dynamic import e configura o map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -4,7 +4,11 @@ import React from 'react'
 
 const Map = () => {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={[0, 0]}
+      zoom={3}
+      style={{ height: '100%', width: '100%' }}
+    >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
