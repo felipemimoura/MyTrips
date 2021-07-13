ef5f433eab8b9ce8d5de4a54c3c8d2b26db1323f
Felipe Moura <felipe.moura@interprocess.com.br>
2021-07-13 20:51:55 -0300

    feat: criado rotas dinâmicas tráves do conteúdo da api

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,30 @@
-import AboutTemplate from 'template/about'
+import client from 'graphQL/client'
+import { GET_PAGES } from 'graphQL/queries'
+import { useRouter } from 'next/dist/client/router'
+import PagesTemplate from 'template/Pages'
 
-const About = () => <AboutTemplate />
+export default function AboutPage() {
+  const router = useRouter()
 
-export default About
+  if (router.isFallback) return null
+
+  return <PagesTemplate />
+}
+
+export async function getStaticPaths() {
+  const { pages } = await client.request(GET_PAGES, { first: 3 })
+
+  const paths = pages.map(({ slug }) => ({
+    params: { slug }
+  }))
+
+  return { paths, fallback: true }
+}
+
+// export const getStaticProps = async () => {
+//   console.log(pages)
+
+//   return {
+//     props: {}
+//   }
+// }
