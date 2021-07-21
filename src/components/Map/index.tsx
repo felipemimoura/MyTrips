a44c56e130b26ae543a0f6fa8bf86e068473ea64
Felipe Moura <felipe.moura@interprocess.com.br>
2021-07-21 19:57:25 -0300

    Mapa personalizado com mapbox

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -6,16 +6,33 @@ import { MapProps } from './interface'
 const Map = ({ places }: MapProps) => {
   const router = useRouter()
 
+  const MAPBOX_API_KEY = process.env.NEXT_PUBLIC_MAPBOX_API_KEY
+  const MAPBOX_USERID = process.env.NEXT_PUBLIC_MAPBOX_USERID
+  const MAPBOX_STYLEID = process.env.NEXT_PUBLIC_MAPBOX_STYLEID
+
+  //api.mapbox.com/styles/v1/felipemoura/ckrcp61iw093r17qxc5qfwtyd.html?fresh=true&title=view&access_token=pk.eyJ1IjoiZmVsaXBlbW91cmEiLCJhIjoiY2tnNnBlZmNuMTZsMTJya2h1OWVuYWY3ZyJ9._Alt2dQPlpOCk8uJujlbuQ
+  const CustomTileLayer = () => {
+    console.log(MAPBOX_API_KEY)
+    return MAPBOX_API_KEY ? (
+      <TileLayer
+        attribution='© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
+      />
+    ) : (
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    )
+  }
+
   return (
     <MapContainer
       center={[0, 0]}
       zoom={3}
       style={{ height: '100%', width: '100%' }}
     >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <CustomTileLayer />
       {places?.map(({ id, name, slug, location }) => {
         const { latitude, longitude } = location
         return (
